36f3397c31f38d8a8f429c646d78109351042552
ivankotut <ivankotut@gmail.com>
2015-11-01 13:28:59 +0300

    Добавлены новые свойства

--- Dom.js
+++ Dom.js
@@ -2,38 +2,135 @@
  * Created by ivankotut on 27.10.2015.
  */
 
-(function(window){
+if( typeof Dom !== 'undefined'){
+    throw new Error('Нужная переменная занята!');
+}
+
+
+var Dom = (function(){
     'use strict';
 
-    var Dom = window.Dom = {},
-        addProperty;
+    var AddProperty; // прототип с доп свойствами для нашего обьекта
 
-    addProperty = function(obj){
-        return {
-            el    : obj,
-            style : obj.style,
-            hide  : function( fn ){
-                var callback = fn || function(){};
-                obj.style.display = 'none';
-                callback.apply(this,arguments);
-                return Dom;
-            },
-            show  : function( fn ){
-                var callback = fn || function(){};
-                obj.style.display = 'block';
-                callback.apply(this,arguments);
-                return Dom;
-            },
-            callback: function( fn ){
-                var callback = fn || function(){};
-                callback.apply(this,arguments);
-                return Dom;
+    function errorLog(mess){
+        throw new Error(mess);
+    }
+
+    AddProperty = function(obj){
+        this.el = obj;
+        this.parent = obj.parentNode;
+    };
+
+    AddProperty.prototype = {
+        author: 'ivankotut',
+        version: '0.0.1',
+        constructor: AddProperty,
+        events: {},
+        style: function(property){
+            var style = this.el.style;
+
+            if(property === undefined){
+                return style;
             }
+
+            if(typeof property !== 'object'){
+                throw new Error('Нужен обьект!');
+            }
+
+            for(var i in property) if(property.hasOwnProperty(i)){
+                style[i] = property[i];
+            }
+            return this;
+        },
+        click: function(callback){
+            var self = this;
+            this.events.click = function(){
+                callback.apply(self,arguments);
+            };
+            this.el.addEventListener('click', self.events.click);
+        },
+        unClick: function(){
+            var self = this;
+            this.el.removeEventListener('click',self.events.click);
+        },
+        getParent: function(search){
+            var parent = this.el.parentNode;
+
+            // ищем ближайшего родителя по названию тега
+            if(typeof search !== 'string'){
+                return this.parent;
+            }
+
+            var query = search.split('').slice(1).join('');
+
+            switch (search.trim()[0]){
+                case '.':
+                    return this.getParentByClass(query,parent);
+                    break;
+                case '#':
+                    return this.getParentById(query,parent);
+                    break;
+                default :
+                    return this.getParentByTag(search,parent);
+            }
+        },
+        getParentByTag: function(tag){
+            var parent = arguments[1];
+
+            if(typeof parent !== 'object'){
+                this.getParentByTag(tag,this.parent);
+            }
+
+            if(!parent.tagName){
+                errorLog('Не найден такой родитель');
+            }
+
+            if(parent.tagName === tag.toUpperCase()){
+                return parent;
+            } else{
+                return this.getParentByTag(tag,parent.parentNode)
+            }
+        },
+        getParentByClass: function(className){
+            var parent = arguments[1];
+
+            if(typeof parent !== 'object'){
+                this.getParentByClass(className,this.parent);
+            }
+
+            if(parent.className === 'undefined'){
+                errorLog('Не найден такой родитель');
+            }
+
+            if(parent.className.toLowerCase() === className.toLowerCase()){
+                return parent;
+            }
+
+            return this.getParentByClass(className,parent.parentNode)
+        },
+        getParentById: function(id){
+            var parent = arguments[1];
+
+            if(typeof parent !== 'object'){
+                this.getParentById(id,this.parent);
+            }
+
+            if(parent.id === 'undefined'){
+                errorLog('Не найден такой родитель');
+            }
+
+            if(parent.id.toLowerCase() === id.toLowerCase()){
+                return parent;
+            }
+
+            return this.getParentById(id,parent.parentNode)
         }
     };
 
-    Dom.getById = function(id){
-        return addProperty(document.getElementById(id));
+    return {
+        getById: function(id){
+            return new AddProperty(document.getElementById(id));
+        }
     };
 
-}(this));
+}());
--- README.md
+++ README.md
@@ -1,6 +1,53 @@
 # Dom
+Не большая либа для работы с dom api. В большей степени написана ради усвоения этой темы. Version 0.0.1
+#### Примеры
+#
+###### Поиск по id
+#
+```sh
+Dom.getById('link');
+```
 
-Не большая либа для работы с dom api. В большей степени написана ради усвоения этой темы.
+###### Добавить оброботку события клик и удалить обработчик
+#
 
-### Version
-0.0.1
+```sh
+Dom.getById('link').click(function(e){
+   e.preventDefault();
+   console.log(this); // текущий обьект (Dom.getById('link'))
+   this.unClick(); // Удалить оброботчик клика
+});
+
+Dom.getById('link').unClick(); // Или так можно удалить обработчик клика
+```
+
+###### Доступные свойства в выбронном обьекте:
+#
+###### style
+#
+```sh
+// Без параметров возвращает ссылку на свойство style обьекта
+var linkStyle = Dom.getById('link').style();
+linkStyle.background = 'red';
+
+// В качестве параметра может принимать оъект (свойство: значение)
+
+Dom.getById('link').click(function(e){
+      e.preventDefault();
+     this.style({
+         background: 'red',
+         color: 'rgba(255,255,255,0.7)'
+     });
+});
+```
+
+###### parent
+#
+```sh
+// Вертает родителя по заданно му параметру
+this.getParent('div'); // по имени тега
+this.getParent('.test'); // по имени класа
+this.getParent('#test'); // по id
+ // также в this доступен ближайшый родитель
+console.log(this.parent);
+```
